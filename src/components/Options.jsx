f52c5dc2dea255b2310c45ca914f160956371fa9
RoiFuries <roi.furies@gmail.com>
2019-11-09 19:06:24 -0600

    modificacion de header en sbx
    Erik me estuvo ayudando a hacer mi primer componente y fue el header, modificamos el logo

---
+++ src/components/Options.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Options = () => (
+  <>
+    <i className='fas fa-shopping-cart'>
+      <span className='icon__badge'>+9</span>
+    </i>
+    <i className='fas fa-bars' />
+  </>
+);
+
+export default Options;
+
+
+
